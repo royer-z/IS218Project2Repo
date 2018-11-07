d085a0eb75187b15fbcddcb7c1ea5a9d3bba7c09
Royer <rvz2@njit.edu>
2018-11-06 23:37:36 -0500

    Added CSS to login, registration, and question forms

--- questionForm.php
+++ questionForm.php
@@ -3,10 +3,10 @@ session_start();
 $email = $_SESSION['email'];
 // Display the add question form to the user
 echo "<!doctype html><html>";
-echo "<head><meta charset='utf-8'><title>Add Question Form</title></head>";
-echo "<body><form action='newQuestion.php' method='post'>";
-echo "<h1>New Question:</h1><input type='text' name='questionName' placeholder='Question name'><br>";
+echo "<head><meta charset='utf-8'><title>Add Question Form</title><link href='main.css' type='text/css' rel='stylesheet'></head>";
+echo "<body><div class='formContainer'><div class='formBox'><form action='newQuestion.php' method='post'>";
+echo "<h1 class='formHeading'>Add a question!</h1><input type='text' name='questionName' placeholder='Question name'><br>";
 echo "<textarea name='questionBody' placeholder='Question body'></textarea><br>";
 echo "<textarea name='questionSkills' placeholder='Enter multiple skills separated by commas'></textarea><br>";
-echo "<input type='submit' value='Add Question'><br>";
-echo "</form></body></html>";
+echo "<input type='submit' class='formButton' value='Add'><br>";
+echo "</form></div></div></body></html>";
